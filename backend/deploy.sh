7bab8e90e4bb7ea317ab6f0645bf4890a8da9469
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-04-08 16:21:01 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
-#чтобы скрипт завершался, если есть ошибки
+#Если свалится одна из команд, рухнет и весь скрипт
 set -xe
-#скачиваем артефакт
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}/sausage-store-klimachev-sergey-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
-#запускаем java-приложение в фоне
-nohup "java -jar sausage-store-${VERSION}.jar --server.port=9090 &" 
+#Перезаливаем дескриптор сервиса на ВМ для деплоя
+sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+sudo rm -f /home/jarservice/sausage-store.jar||true
+#Переносим артефакт в нужную папку
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-klimachev-sergey-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+sudo cp ./sausage-store.jar /home/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
+#Обновляем конфиг systemd с помощью рестарта
+sudo systemctl daemon-reload
+#Перезапускаем сервис сосисочной
+sudo systemctl restart sausage-store-backend 
