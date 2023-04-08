952f32b0206ae9c854c8f311ae9f7063e9e7eda7
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-04-08 20:08:46 +0000

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -5,12 +5,12 @@ set -xe
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt -y install nodejs
 sudo cp -rf sausage-store-frontend.service /etc/systemd/system/sausage-store-frontend.service
-sudo mkdir -p /var/www-data/
-sudo rm -fR /var/www-data/* ||true
+sudo mkdir -p /var/www-data/dist/frontend/
+sudo rm -fR /var/www-data/dist/frontend/* ||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL}/sausage-store-klimachev-sergey-frontend/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
 tar xvf sausage-store.tar.gz
-sudo cp ./sausage-store-${VERSION}/public_html/* /var/www-data/ ||true
+sudo cp ./sausage-store-${VERSION}/public_html/* /var/www-data/dist/frontend/ ||true
 sudo npm install -g http-server
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
