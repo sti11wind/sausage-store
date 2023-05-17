ebd71d31cdfc96f1519b920a83a173320297693d
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-05-17 09:50:41 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,15 +7,15 @@ sudo rm -f /var/jarservice/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-klimachev-sergey-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /var/jarservice/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
-echo PSQL_HOST=${PSQL_HOST} > /var/jarservice/vars
-echo PSQL_PORT=${PSQL_PORT} >> /var/jarservice/vars
-echo PSQL_DBNAME=${PSQL_DBNAME} >> /var/jarservice/vars
-echo PSQL_USER=${PSQL_USER} >> /var/jarservice/vars
-echo PSQL_PASSWORD=${PSQL_PASSWORD} >> /var/jarservice/vars
+echo PSQL_HOST=${PSQL_HOST} > /home/jarservice/vars
+echo PSQL_PORT=${PSQL_PORT} >> /home/jarservice/vars
+echo PSQL_DBNAME=${PSQL_DBNAME} >> /home/jarservice/vars
+echo PSQL_USER=${PSQL_USER} >> /home/jarservice/vars
+echo PSQL_PASSWORD=${PSQL_PASSWORD} >> /home/jarservice/vars
 echo MONGO_USER=${MONGO_USER} >> /vars/jarservice/vars
-echo MONGO_PASSWORD=${MONGO_PASSWORD} >> /var/jarservice/vars
-echo MONGO_HOST=${MONGO_HOST} >> /var/jarservice/vars
-echo MONGO_DATABASE=${MONGO_DATABASE} >> /var/jarservice/vars
+echo MONGO_PASSWORD=${MONGO_PASSWORD} >> /home/jarservice/vars
+echo MONGO_HOST=${MONGO_HOST} >> /home/jarservice/vars
+echo MONGO_DATABASE=${MONGO_DATABASE} >> /home/jarservice/vars
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
