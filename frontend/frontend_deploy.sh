0d8a395a18cb2fe30ed108c0f722fd107e1494ac
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-05-21 08:19:40 +0000

    Update frontend_deploy.sh

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,13 +1,13 @@
-docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
 #!/bin/sh
-set +e
+set +ex
 cat > .env <<EOF
 EOF
+docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
 docker network create -d bridge sausage_network || true
 docker pull gitlab.praktikum-services.ru:5050/std-014-65/sausage-store/sausage-frontend:latest
 docker stop frontend || true
 docker rm frontend || true
-set -e
+set -ex
 sudo docker run -d --name frontend \
     --network=sausage_network \
     --restart always \
