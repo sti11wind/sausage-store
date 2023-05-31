e3cfeb50f0d7cbc7141c5fdae2d97ae2a0fd9410
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-05-31 17:46:00 +0000

    Update backend-report_deploy.sh

--- backend-report/backend-report_deploy.sh
+++ backend-report/backend-report_deploy.sh
@@ -4,9 +4,7 @@ cat > .env <<EOF
 DB=${SPRING_DATA_MONGODB_URI}
 PORT=8080
 VAULT_ADDR=${VAULT_ADDR}
-SSL_CERT_FILE=YandexInternalRootCA.pem
 EOF
-curl https://storage.yandexcloud.net/cloud-certs/CA.pem -o YandexInternalRootCA.crt
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker network create -d bridge sausage_network || true
 docker pull ${CI_REGISTRY}/${CI_PROJECT_NAMESPACE}/sausage-store/sausage-backend-report:latest 
