bd3ed787975ddbac10a9d7d3117220456c86cea8
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-05-31 17:20:07 +0000

    Update backend-report_deploy.sh

--- backend-report/backend-report_deploy.sh
+++ backend-report/backend-report_deploy.sh
@@ -4,6 +4,7 @@ cat > .env <<EOF
 DB=${SPRING_DATA_MONGODB_URI}
 PORT=8080
 VAULT_ADDR=${VAULT_ADDR}
+SSL_CERT_FILE=YandexInternalRootCA.pem
 EOF
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
 docker network create -d bridge sausage_network || true
