b51caba48f635ed832dcf35880ae2ab537d9d1f1
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-06-01 20:57:43 +0000

    Update frontend_deploy.sh

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker login -u $MY_DOCKER_USER -p $MY_DOCKER_PASS
-docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASSWORD} ${REGISTRY} || true
 docker compose up -d  --force-recreate --pull  always frontend
 
 
