0ea3e8ff48a6a367d250ed26159ae1ec793a5667
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-06-01 18:31:20 +0000

    Update frontend_deploy.sh

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -5,7 +5,6 @@ VERSION=${VERSION}
 EOF
 docker login -u $MY_DOCKER_USER -p $MY_DOCKER_PASS
 docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-docker pull ${CI_REGISTRY}/${CI_PROJECT_NAMESPACE}/sausage-store/sausage-frontend:latest
 docker-compose stop frontend
 docker-compose rm -f frontend
 docker-compose create frontend
