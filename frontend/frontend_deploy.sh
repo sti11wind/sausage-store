87af9635939a63440488bb18181cc63c00436239
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-06-01 19:08:09 +0000

    Update frontend_deploy.sh

--- frontend/frontend_deploy.sh
+++ frontend/frontend_deploy.sh
@@ -3,12 +3,10 @@ set +e
 cat > .env <<EOF
 VERSION=${VERSION}
 EOF
-sudo docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-sudo docker pull gitlab.praktikum-services.ru:5050/std-014-65/sausage-store/sausage-frontend:$VERSION
-sudo docker-compose stop frontend
-sudo docker-compose rm -f frontend
-sudo docker-compose create frontend
-sudo docker-compose start frontend
+docker login -u $MY_DOCKER_USER -p $MY_DOCKER_PASS
+docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+docker-compose -d  --force-recreate --pull  always frontend
+
 
 
 
