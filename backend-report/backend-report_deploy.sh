bd376a5d498628e00c916094c4d71b1301870010
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-06-02 18:11:43 +0000

    Update backend-report_deploy.sh

--- backend-report/backend-report_deploy.sh
+++ backend-report/backend-report_deploy.sh
@@ -5,15 +5,7 @@ DB=${SPRING_DATA_MONGODB_URI}
 PORT=8080
 VAULT_ADDR=${VAULT_ADDR}
 EOF
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-docker network create -d bridge sausage_network || true
-docker pull ${CI_REGISTRY}/${CI_PROJECT_NAMESPACE}/sausage-store/sausage-backend-report:latest 
-docker stop backend-report || true
-docker rm backend-report || true
-set -e
-docker run -d --name backend-report \
-    --network=sausage_network \
-    --restart always \
-    --pull always \
-    --env-file .env \
-    ${CI_REGISTRY}/${CI_PROJECT_NAMESPACE}/sausage-store/sausage-backend-report:latest
+docker login -u ${MY_DOCKER_USER} -p ${MY_DOCKER_PASS}
+docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASSWORD} ${REGISTRY} || true
+docker compose up -d  --force-recreate --pull  always backend-report
+
