20eea863080048624e90593141f580b2c457114f
std-014-65 std-014-65 <std-014-65@praktikum-services.ru>
2023-06-22 17:05:53 +0000

    Update backend-report_deploy.sh

--- backend-report/backend-report_deploy.sh
+++ backend-report/backend-report_deploy.sh
@@ -3,7 +3,6 @@ set +e
 cat > .env_report <<EOF
 DB=${SPRING_DATA_MONGODB_URI}
 PORT=8080
-VAULT_ADDR=${VAULT_ADDR}
 EOF
 docker login -u ${MY_DOCKER_USER} -p ${MY_DOCKER_PASS}
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY} || true
